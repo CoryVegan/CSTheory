2103bdd60d10dd03e44887ac392bb3e49800c064
Lvsi-China <35942268+lvsi-china@users.noreply.github.com>
2018-02-02 20:58:08 +0800

    主分支的第 1 次更新

---
+++ practice-generics/generice-class/src/House.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// 定义泛型类 : 房屋
+public class House<T> {
+
+    private List<T> arrayList = new ArrayList<T>() ;
+
+
+    public void put( T t ){
+
+        this.arrayList.add( t ) ;
+    }
+
+
+
+    public T get( int index ){
+
+        return this.arrayList.get( index ) ;
+    }
+
+    public List<T> getArrayList(){
+
+        return this.arrayList ;
+    }
+
+}
+
+
+
+
+
+
+
